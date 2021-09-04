c169e5c30075dc5bbeb3edb67da02a5090c6abc9
Taci Xat <cybering@hey.com>
2021-09-04 01:12:00 -0700

    Base16.

--- _sssg/main.go
+++ _sssg/main.go
@@ -97,7 +97,7 @@ func genPosts(id *IndexData) {
 
 		cr := NewChromaRenderer(
 			ChromaOptions(html.TabWidth(4)))
-		// Style("native")(cr)
+		Style("base16-snazzy")(cr)
 		pd.Raw = template.HTML(blackfriday.Run(
 			md, blackfriday.WithRenderer(cr)))
 		writeTemplate(
